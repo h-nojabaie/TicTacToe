e9adc1a996cfa174c664885e30b9d3fac529eb78
hanieh nojabaie <nojabaiehanieh@gmail.com>
2022-07-16 15:44:57 +0430

    Delete some additional codes

--- tictaktoe.py
+++ tictaktoe.py
@@ -90,11 +90,6 @@ class TicTacToe:
         
         return False
 
-#         for row in self.board:
-#             for item in row:
-#                 if item == '-':
-#                     return False
-#         return True
 
     def is_board_filled(self):
         for row in self.board:
